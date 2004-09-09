0f71333353c763b58d3d147dda3ffa630956820c
c_froehlich <c_froehlich@c8bd249d-02ed-0310-b6d2-cb45343a2160>
2004-09-09 13:05:00 +0000

    initial import

---
+++ trunk/semantag/src/test/net/sf/semantag/tm/BaseUseTagTest.java
@@ -0,0 +1,114 @@
+package net.sf.semantag.tm;
+
+import org.apache.commons.jelly.JellyTagException;
+import org.apache.commons.jelly.MissingAttributeException;
+import org.apache.commons.jelly.XMLOutput;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ * @author cf
+ * @version 0.1, created on 09.09.2004
+ */
+public class BaseUseTagTest extends TestCase {
+
+    BaseUseTag but ;
+    
+    protected void setUp() throws Exception {
+        
+        // make a non abstract instance
+        but = new BaseUseTag(){
+            public void doTag(XMLOutput output) throws MissingAttributeException,
+            JellyTagException {
+            }    
+               
+        };
+    }
+    
+    // checks proper storage of id and sourceLocator-Attributes
+    public void testStoreProperties() {
+        String idValue = "v";
+        String slValue = "sl";
+        
+        assertNull(but.getId());
+        assertNull(but.getSourceLocator());
+        
+        but.setId(idValue);
+        but.setSourceLocator(slValue);
+        
+        assertEquals(idValue, but.getId());
+        assertEquals(slValue, but.getSourceLocator());
+    }
+
+
+    public void testNonexistantModes() {
+        String mode;
+        
+        // the tag uses initially the default mode
+        assertEquals(but.getNonexistant(), BaseUseTag.NE_DEFAULT);
+        
+        // check FAIL, upper and lower case
+        mode = BaseUseTag.NE_FAIL;
+        checkNonExistantModes(mode.toLowerCase(), mode, true, false, false);
+        checkNonExistantModes(mode.toUpperCase(), mode, true, false, false);
+        
+        // check ADD, upper and lower case
+        mode = BaseUseTag.NE_ADD;
+        checkNonExistantModes(mode.toLowerCase(), mode, false, true, false);
+        checkNonExistantModes(mode.toUpperCase(), mode, false, true, false);
+        
+        // check IGNORE, upper and lower case
+        mode = BaseUseTag.NE_IGNORE_BODY;
+        checkNonExistantModes(mode.toLowerCase(), mode, false, false, true);
+        checkNonExistantModes(mode.toUpperCase(), mode, false, false, true);
+        
+        // check that unknown mode causes the
+        // default to be used
+        // default is assumed to be IGNORE
+        mode = "foo";
+        checkNonExistantModes(mode.toLowerCase(), BaseUseTag.NE_IGNORE_BODY, false, false, true);
+
+        // check that null mode causes the
+        // default to be used
+        // default is assumed to be IGNORE
+        mode = null;
+        checkNonExistantModes(mode, BaseUseTag.NE_IGNORE_BODY, false, false, true);
+
+        
+    }
+    /**
+     * Checks that setting the nonexistant - property of the
+     * BaseUseTag to <code>mode</code> leads to the following
+     * results.
+     * <ul>
+     *  <li>shallFailOnNonexistant() equals to <code>fail</code></li>
+     *  <li>shallAddOnNonexistant() equals to <code>add</code></li>
+     *  <li>shallIgnoreOnNonexistant() equals to <code>ignore</code></li>
+     *  <li>getNonexistant() equals to <code>storedMode</code></li>
+     * </ul>
+     * @param mode the mode to pass in BaseUseTag
+     * @param storedMode the mode that BaseUseTag returns
+     * @param fail the expected result of shallFailOnNonexistant
+     * @param add the expected result of shallAddOnNonexistant
+     * @param ignore the expected result of shallIgnoreOnNonexistant
+     */
+    private void checkNonExistantModes(String mode, String storedMode,
+            boolean fail, boolean add, boolean ignore){
+       
+        
+        // set mode 
+        but.setNonexistant(mode);
+        
+        //check if the shortcut methods behave as expected
+        assertEquals(fail, but.shallFailOnNonexistant());
+        assertEquals(add, but.shallAddOnNonexistant());
+        assertEquals(ignore, but.shallIgnoreOnNonexistant());
+
+        // check if the string that is stored for the mode
+        // is as expected
+        assertEquals(storedMode, but.getNonexistant());
+        
+    }
+    
+}
---
+++ trunk/semantag/src/test/net/sf/semantag/tm/TMOResolverTest.java
@@ -0,0 +1,74 @@
+package net.sf.semantag.tm;
+import net.sf.semantag.TestData;
+
+import org.apache.commons.jelly.JellyContext;
+import org.tm4j.net.Locator;
+import org.tm4j.topicmap.TopicMap;
+import org.tm4j.topicmap.TopicMapObject;
+
+
+/**
+ * 
+ * @author cf
+ * @version 0.1, created on 06.09.2004
+ */
+public class TMOResolverTest extends BaseTMTagTest {
+
+    private final static String baseLoc = TestData.TM_GREEKS_BASELOCATOR;
+
+    TopicMap tm;
+    TMOResolver resolver;
+
+   
+
+    // stores a topic map object in the context and uses
+    // the TMOResolver to retrieve it by key name
+    public void testIdentifyByVariable() 
+        throws Exception
+    {
+        String id = "as00001";
+        String varName = "TMO";
+
+        TopicMapObject tmo = tm.getObjectByID(id);
+        assertNotNull(tmo);
+        JellyContext ctx = new JellyContext();
+        ctx.setVariable(varName, tmo);
+        
+        resolver.setVariable(varName);
+        assertEquals(tmo, resolver.getTopicMapObject(tm, ctx));
+        
+    }
+
+    // sets the id and tests whether the
+    // TMOResolver returns the expected topic map object
+    public void testIdentifyById() 
+        throws Exception
+    {
+        String id = "name_of_maia";
+        resolver.setId(id);
+        TopicMapObject tmo = resolver.getTopicMapObject(tm, new JellyContext());
+        String adress = ((Locator)tmo.getSourceLocators().iterator().next()).getAddress();
+        assertEquals(baseLoc+"#"+id,adress);
+    }
+
+    // sets the sourceLocator and tests whether the
+    // TMOResolver returns the expected topic map object
+    public void testIdentifyBySourceLocator() 
+        throws Exception
+    {
+        String id = "name_of_maia";
+        String adress = baseLoc+"#"+id; 
+        resolver.setSourceLocator(adress);
+
+        TopicMapObject tmo = resolver.getTopicMapObject(tm, new JellyContext());
+        assertEquals(id,tmo.getID());
+    }
+
+
+
+    protected void setUp() throws Exception {
+        tm = getTopicMapFromResource(TestData.TM_GREEKS_XTM, baseLoc);
+        resolver = new TMOResolver();
+
+    }
+}
